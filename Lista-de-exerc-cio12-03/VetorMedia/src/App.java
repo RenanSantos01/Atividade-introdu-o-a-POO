4dc425136161c8ddc2701c5af108ff912766230d
Renan Santos <renancardoso1414@gmail.com>
2024-07-16 18:45:05 -0300

    ok

---
+++ Lista-de-exerc-cio12-03/VetorMedia/src/App.java
@@ -0,0 +1,17 @@
+//tirar a media do vetor
+
+public class App {
+    public static void main(String[] args) throws Exception {
+       
+        System.out.println("testando");
+    
+    int x = 5;
+    int y = 10;
+    
+    int soma = x + y;
+
+    System.out.println(soma);
+    
+    
+    }
+}
